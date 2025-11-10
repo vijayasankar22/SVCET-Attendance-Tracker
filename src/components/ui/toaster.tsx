0eb884ebfd1c33ef56a86f6ddbbfe60fa8a136fb
Vijayasankar <vijayasankar2225@gmail.com>
2025-11-10 07:37:10 +0000

    still it is samke like that

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -18,16 +18,18 @@ export function Toaster() {
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
-            <div className="flex items-center justify-between w-full">
+            <div className="flex items-center justify-between w-full gap-4">
               <div className="grid gap-1">
                 {title && <ToastTitle>{title}</ToastTitle>}
                 {description && (
                   <ToastDescription>{description}</ToastDescription>
                 )}
               </div>
-              {action}
+              <div className="flex items-center gap-2">
+                {action}
+                <ToastClose />
+              </div>
             </div>
-            <ToastClose />
           </Toast>
         )
       })}
